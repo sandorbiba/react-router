bd06032a4d4a1586734fa3b7ccac569425d20cf8
Sandor Biba <sandorbiba@gmail.com>
2019-06-27 18:44:16 +0200

    final

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+import About from "./About";
+import Dog from "./Dog";
+import Contact from "./Contact";
+import "./App.css";
+
+const Hater = () => <h1>I AM A HATER!</h1>;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav className="App-nav">
+          <NavLink activeClassName="active-link" to="/">
+            About
+          </NavLink>
+          <NavLink activeClassName="active-link" to="/contact">
+            Contact
+          </NavLink>
+          <NavLink activeClassName="active-link" to="/dog">
+            Dog
+          </NavLink>
+
+          <NavLink activeClassName="active-link" to="/dog/r">
+            Dog(r)
+          </NavLink>
+          <NavLink activeClassName="active-link" to="/dog/c">
+            Dog(c)
+          </NavLink>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/dog" component={Dog} />
+          <Route exact path="/dog/hater" component={Hater} />
+          <Route exact path="/dog/c" component={() => <Dog name="Muffins" />} />
+          <Route exact path="/dog/r" render={() => <Dog name="Biscuits" />} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
